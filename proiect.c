2017a93fe43d924e4f0123ef8d24d09d7344090d
Tibi7110 <tibichirila2004@gmail.com>
2025-03-09 00:51:09 +0200

    pr

--- proiect.c
+++ proiect.c
@@ -4,36 +4,37 @@
 
 void add_password()
 {
-    char password[256] = "", encrypt[] = "&*jnkl(!.,)", temp, output[280] = "";
+    unsigned char password[256] = "", encrypt[] = "&*jnkl(!.,)", output[280] = "";
     printf("\nAdd password:");
     scanf("%256s", password);
+    //printf("\nWhat is the password for?:");
     FILE *f = fopen("baza_date", "a");
     strcat(password, "!njdsj");
-    int length_encrypt = strlen(encrypt), length_output = strlen(password), j = 0;
+    int length_encrypt = strlen(encrypt), length_output = strlen(password), j = 0, temp;
     for(int i = 0; i < length_output; i++) {
         if(j == length_encrypt)
             j = 0;
-        temp = password[i] ^ encrypt[j++];
-        output[i] = temp;
+        temp = (password[i] - '0') ^ (encrypt[j++] - '0');
+        output[i] = temp + '0';
     }
-    fprintf(f,"%d %s ",length_output, output);
+    fprintf(f,"%d %s ",length_output - 6, output);
     fclose(f);
 }
 
 void decrypt()
 {
-    char password[280] = "", encrypt[] = "&*jnkl(!.,)", temp, output[280] = "";
+    char password[280] = "", encrypt[] = "&*jnkl(!.,)", output[280] = "";
     FILE *f = fopen("baza_date", "r");
-    int length_encrypt = strlen(encrypt), length_output, j = 0;
+    int length_encrypt = strlen(encrypt), length_output, j = 0, temp;
     while(fscanf(f,"%d %s", &length_output, password) == 2) {
         j = 0;
         for(int i = 0; i < length_output; i++) {
             if(j == length_encrypt)
             j = 0;
-            temp = password[i] ^ encrypt[j++];
-            output[i] = temp;
+            temp = (password[i] - '0') ^ (encrypt[j++] - '0');
+            output[i] = temp + '0';
         }
-        for(int i = 0; i < length_output - 6; i++)
+        for(int i = 0; i < length_output; i++)
         printf("%c", output[i]);
         printf("\n");
     }
@@ -57,12 +58,12 @@ void menu()
 int main(void)
 {
     char s[256], parola[256];
-    printf("Enter password :");
-    scanf("%256s", s);
-    FILE *f = fopen("parola", "r");
-    fscanf(f,"%256s", parola);
-    fclose(f);
-    if(strcmp(s,parola) == 0)
+   // printf("Enter password :");
+   // scanf("%256s", s);
+   // FILE *f = fopen("parola", "r");
+   // fscanf(f,"%256s", parola);
+   // fclose(f);
+   // if(strcmp(s,parola) == 0)
         menu();
 
     return 0;
