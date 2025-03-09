b8c93e20af441d64cb36f28d7f7b933895f8767e
Tibi7110 <tibichirila2004@gmail.com>
2025-03-09 20:33:01 +0200

    latest version

---
+++ helper.h
@@ -0,0 +1,14 @@
+#ifndef helper
+#define helper
+
+void add_password();
+
+void encrypt(char password[]);
+
+void erase();
+
+void decrypt();
+
+void gen_password();
+
+#endif
--- proiect.c
+++ proiect.c
@@ -2,77 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-
-void erase()
-{
-    FILE *f = fopen("baza_date", "w");
-    fclose(f);
-}
-
-void encrypt(char password[])
-{
-    char encrypt[] = "&*jnkl(!.,)", output[280] = "";
-    FILE *f = fopen("baza_date", "a");
-    strcat(password, "!njdsj");
-    int length_encrypt = strlen(encrypt), length_output = strlen(password), j = 0, temp;
-    for(int i = 0; i < length_output; i++) {
-        if(j == length_encrypt)
-            j = 0;
-        temp = (password[i] - '0') ^ (encrypt[j++] - '0');
-        output[i] = temp + '0';
-    }
-    fprintf(f,"%d %s ",length_output - 6, output);
-    fclose(f);
-}
-
-void add_password()
-{
-    char password[256], site[50];
-    printf("Add password: ");
-    scanf("%256s", password);
-    encrypt(password);
-    printf("What is the password for? ");
-    scanf("%50s", site);
-    FILE *f = fopen("baza_date", "r");
-    fprintf(f, " - %s ", site);
-    fclose(f);
-}
-
-void decrypt()
-{
-    char password[280] = "", encrypt[] = "&*jnkl(!.,)", output[280] = "", site[50], t[5];
-    FILE *f = fopen("baza_date", "r");
-    int length_encrypt = strlen(encrypt), length_output, j = 0, temp;
-    while(fscanf(f,"%d %s %s %s", &length_output, password, t, site) == 2) {
-        j = 0;
-        for(int i = 0; i < length_output; i++) {
-            if(j == length_encrypt)
-            j = 0;
-            temp = (password[i] - '0') ^ (encrypt[j++] - '0');
-            output[i] = temp + '0';
-        }
-        for(int i = 0; i < length_output; i++)
-        printf("%c", output[i]);
-        printf(" - %s", site);
-    }
-    fclose(f);
-}
-
-void gen_password()
-{
-    srand(time(NULL));
-    int min = 8, max = 16, length;
-    length = rand() % (max - min + 1) + min;
-    char password[20], site[50];
-    for(int i = 0; i < length; i++)
-        password[i] = '0' + rand() % 74;
-    encrypt(password);
-    printf("What is the password for? ");
-    scanf("%50s", site);
-    FILE *f = fopen("baza_date", "r");
-    fprintf(f, " - %s ", site);
-    fclose(f);
-}
+#include "helper.h"
 
 void menu()
 {
